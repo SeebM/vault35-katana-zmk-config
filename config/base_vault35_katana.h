a3984fb3ba55ca2ad3dd084528de9684df65f276
Nico Stuhlmueller <n+git@ncxst.de>
2025-05-25 11:06:25 +0200

    added combos

--- config/base_vault35_katana.h
+++ config/base_vault35_katana.h
@@ -3,6 +3,7 @@
 #include <dt-bindings/zmk/keys.h>
 #include <dt-bindings/zmk/outputs.h>
 
+#define APT 0
 #define NAV 1
 #define NUM 2
 #define SYM 3
@@ -12,80 +13,54 @@
     continue-list = <UNDERSCORE MINUS BSPC LSHFT RSHFT>;
 };
 
+/ {
+  macros {
+  macro_sch:
+    macro_sch {
+      label = "Macro_sch";
+      compatible = "zmk,behavior-macro";
+      #binding-cells = <0>;
+      bindings = <&macro_tap &kp S &macro_release &kp RSHIFT &macro_tap &kp C &kp H>;
+    };
+  };
+};
+
+#define COMBO(NAME, BINDINGS, KEYPOS) \
+  combo_##NAME {                      \
+    timeout-ms = <20>;                \
+    bindings = <BINDINGS>;            \
+    key-positions = <KEYPOS>;         \
+  };
 
 / {
     combos {
         compatible = "zmk,combos";
-        /* parentheticals */
-        combo_lbrc {
-            timeout-ms = <40>;
-            key-positions = <1 11>;
-            bindings = <&kp DE_LBRC>;
-        };
-        combo_lbkt {
-            timeout-ms = <40>;
-            key-positions = <2 12>;
-            bindings = <&kp DE_LBKT>;
-        };
-        combo_lpar {
-            timeout-ms = <40>;
-            key-positions = <3 13>;
-            bindings = <&kp DE_LPAR>;
-        };
-        combo_lt {
-            timeout-ms = <40>;
-            key-positions = <4 14>;
-            bindings = <&kp DE_LT>;
-        };
-        combo_gt {
-            timeout-ms = <40>;
-            key-positions = <5 15>;
-            bindings = <&kp DE_GT>;
-        };
-        combo_rpar {
-            timeout-ms = <40>;
-            key-positions = <6 16>;
-            bindings = <&kp DE_RPAR>;
-        };
-        combo_rbkt {
-            timeout-ms = <40>;
-            key-positions = <7 17>;
-            bindings = <&kp DE_RBKT>;
-        };
-        combo_rbrc {
-            timeout-ms = <40>;
-            key-positions = <8 18>;
-            bindings = <&kp DE_RBRC>;
-        };
-        /* caps */
-        combo_caps-word {
-            timeout-ms = <40>;
-            key-positions = <13 16>;
-            bindings = <&caps_word>;
-        };
-        combo_capslock {
-            timeout-ms = <40>;
-            key-positions = <3 6>;
-            bindings = <&kp CAPSLOCK>;
-        };
-        /* deletion */
-        combo_bspc {
-            timeout-ms = <40>;
-            key-positions = <6 7>;
-            bindings = <&kp BSPC>;
-        };
-        combo_del {
-            timeout-ms = <40>;
-            key-positions = <7 8>;
-            bindings = <&kp DEL>;
-        };
-        combo_delword {
-            timeout-ms = <40>;
-            key-positions = <6 8>;
-            bindings = <&kp LC(BSPC)>;
-        };
+/* usually on base layer */
+        COMBO(q, &kp Q, 2 3)
+        COMBO(z, &kp DE_Z, 21 22)
+        COMBO(v, &kp V, 1 2)
+        COMBO(b, &kp B, 12 3)
+        COMBO(k, &kp K, 12 13)
+        COMBO(x, &kp X, 16 17)
+        COMBO(j, &kp J, 6 17)
+        COMBO(sch, &macro_sch, 11 13)
+        COMBO(_sz, &kp DE_SZ, 34 11)
+        COMBO(_ue, &kp DE_UE, 34 8)
+        COMBO(_ae, &kp DE_AE, 34 17)
+        COMBO(_oe, &kp DE_OE, 34 19)
+        COMBO(ent, &kp ENTER, 26 27)
+        COMBO(esc, &kp ESC, 23 24)
+/* caps */
+        COMBO(capsword, &caps_word, 13 16)
+        COMBO(capslock, &kp CAPSLOCK, 0 9)
+/* deletion */
+        COMBO(bspc, &kp BSPC, 6 7)
+        COMBO(del, &kp DEL, 7 8)
+        COMBO(bspcword, &kp LC(BSPC), 6 8)
+
     };
 };
+
 
 / {
     behaviors {
